0962b2f451f358a81af76c8594c5b8718ea9e64d
zuodong <zuodong@wolongdata.com>
2020-07-03 08:54:10 +0800

    在flask中以流的形式做上传时，根据文件头来做文件类型判断

---
+++ wsgi.py
@@ -0,0 +1,30 @@
+import magic
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    with open("./upload_file.pdf", "wb") as f:
+        chunk_size = 2048
+        in_times = 0
+        while True:
+            chunk = request.stream.read(chunk_size)
+            in_times += 1
+            if in_times == 1:
+                chunk = chunk.split(b"\r\n\r\n")
+                file_type = magic.from_buffer(chunk[1])
+                print(file_type)
+                f.write(chunk[1])
+                continue
+            if len(chunk) == 0:
+                break
+            else:
+                f.write(chunk)
+    return "ok"
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
